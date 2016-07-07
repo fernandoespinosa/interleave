3ced5c9a37e4f01fc8abe3ac0e20450faaa4eef1
fernandoespinosa <fernandoespinosa@fernandoespinosa.org>
2016-07-07 11:51:25 -0400

    Initial commit.

---
+++ Interleave/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Interleave
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var interleave = File.ReadAllLines("Input/Input-1.txt").Interleave(File.ReadAllLines("Input/Input-2.txt"));
+            foreach (var line in interleave)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Interleave<T>(this IEnumerable<T> e1, IEnumerable<T> e2)
+        {
+            return e1.Zip(e2, (arg1, arg2) => new[] { arg1, arg2 }).SelectMany(t => t);
+        }
+    }
+}
